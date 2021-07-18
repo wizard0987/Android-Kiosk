bf25d1a4b41fe16cdbe3e22ae2d73e7eabc61ae1
leejiwon <leejiwon3737@gmail.com>
2021-07-19 00:29:53 +0900

    시작 Activity 설정 및 대기화면 지연시간 추가

--- app/src/main/java/com/example/megacoffee/SplashActivity.java
+++ app/src/main/java/com/example/megacoffee/SplashActivity.java
@@ -1,15 +1,39 @@
 package com.example.megacoffee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.megacoffee.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding splashBinding;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        // ViewBinding Setting
+//        setContentView(R.layout.activity_splash);
+        splashBinding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = splashBinding.getRoot();
+        setContentView(view);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 4000L);
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
